bb2df8587539ee4d6872b17350d7a77492727d5b
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2022-12-08 13:24:27 +0100

    Add integration with weatherapi.com

--- src/main/kotlin/com/io/reactivecoroutines/weather/WeatherController.java
+++ src/main/kotlin/com/io/reactivecoroutines/weather/WeatherController.java
@@ -2,14 +2,16 @@ package com.io.reactivecoroutines.weather;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @Validated
-@RequestMapping("/weather-infos/")
+@RequestMapping("/weather-infos")
 public class WeatherController {
     private final WeatherService weatherService;
 
@@ -18,11 +20,18 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
+    @GetMapping
     public Flux<WeatherInfo> getAllWeather() {
         return weatherService.getAllWeather();
     }
 
+    @GetMapping("/{id}")
     public Mono<WeatherInfo> getById(String id) {
         return weatherService.getWeatherInfoById(id);
+    }
+
+    @GetMapping("/current")
+    public Mono<WeatherInfo> getCurrentWeatherIn(@RequestParam final String city) {
+        return weatherService.getCurrentWeatherIn(city);
     }
 }
